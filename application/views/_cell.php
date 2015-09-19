6f29e99fc1dc95c6ca6ff234d2b1860bc887cf5e
Evanna Wong <evannawong92@gmail.com>
2015-09-19 00:19:20 -0700

    Transformed into a good CodeIgniter webapp.

---
+++ application/views/_cell.php
@@ -0,0 +1,14 @@
+<?php
+
+/* <a href="./data/serengeti-landscape.jpg" 
+        data-lightbox="gallery" 
+        data-title="Beautiful Landscape of Serengeti ... Posted 2014.04.05 by donald, in landscape">
+        <img src="./data/thumb/serengeti-landscape.jpg"/></a>
+ * 
+ */
+?>
+
+<a href="./data/{filename}" data-lightbox="gallery" data-title="{title}... Posted {uploaded} by {uploader}, in {category}">
+    <img src="./data/thumb/{filename}" />
+</a>
+
